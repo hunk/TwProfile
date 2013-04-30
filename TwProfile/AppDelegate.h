b91ad16696bea207f95407b6ca55adb6cab9ce7e
Edgar g (@hunk) <ing.edgar@gmail.com>
2013-04-30 12:23:39 -0500

    add files

---
+++ TwProfile/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TwProfile
+//
+//  Created by hunk on 4/30/13.
+//  Copyright (c) 2013 mx.com.hunk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ProfileViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ProfileViewController *profileVC;
+
+@end
---
+++ TwProfile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwProfile
+//
+//  Created by hunk on 4/30/13.
+//  Copyright (c) 2013 mx.com.hunk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
